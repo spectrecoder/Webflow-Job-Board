9e2e25a28d458c87a313e06d2bcbd85bc96284b6
spectrecoder <pong96111kind@gmail.com>
2023-05-04 14:55:54 -0500

    update nav

--- pages/jobs/index.jsx
+++ pages/jobs/index.jsx
@@ -30,7 +30,7 @@ export default function Jobs() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
+      <Newnav />
       <main>
         <Hero buttonText={"Find a job"} buttonLink={{ href: "#jobs" }} />
         <Brands />
